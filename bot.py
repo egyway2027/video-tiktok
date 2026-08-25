ffa0fca6ca580e422bce71b9594253d5a6e30c14
egyway2027 <egyway2027@gmail.com>
2026-08-25 03:31:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ import shutil
 import sqlite3
 import subprocess
 import urllib.error
-imp
+import urllib.parse
 import urllib.request
 from datetime import datetime, timedelta
 from pathlib import Path
